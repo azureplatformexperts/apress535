b1e355aa1543fbecff45d11150b4fab8f59c43cf
Chris <christophe.lams@synergics.be>
2018-01-01 15:34:47 +0100

    script update

--- iFreeze/website/configure-IIS.ps1
+++ iFreeze/website/configure-IIS.ps1
@@ -1,6 +1,6 @@
 Add-WindowsFeature Web-Server
-Start-Sleep -s 10
-Remove-Item -Path C:\inetpub\wwwroot\Default.html -Force -
+Start-Sleep -s 30
+Remove-Item -path "C:\inetpub\wwwroot" -Force -Recurse -ErrorAction SilentlyContinue
 $dbsource = "https://ifreezwebartifacts.blob.core.windows.net/downloads/default.html"
 $dbdestination = "C:\inetpub\wwwroot\Default.html"
 Invoke-WebRequest $dbsource -OutFile $dbdestination
