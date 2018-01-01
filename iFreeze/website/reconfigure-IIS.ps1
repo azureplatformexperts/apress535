74d240ea6f66d33a21b3e6d42566ff84fd9163a3
Chris <christophe.lams@synergics.be>
2018-01-01 17:09:52 +0100

    add script to reconfigure
    no IIS install, just files

---
+++ iFreeze/website/reconfigure-IIS.ps1
@@ -0,0 +1,8 @@
+Remove-Item -path "C:\inetpub\wwwroot" -Force -Recurse -ErrorAction SilentlyContinue
+New-Item -ItemType directory -Path "C:\inetpub\wwwroot\"
+$dbsource1 = "https://ifreezwebartifacts.blob.core.windows.net/downloads/default.html"
+$dbsource2 = "https://ifreezwebartifacts.blob.core.windows.net/downloads/ifreezewebsite.gif"
+$dbdestination1 = "C:\inetpub\wwwroot\default.html"
+$dbdestination2 = "C:\inetpub\wwwroot\ifreezewebsite.gif"
+Invoke-WebRequest $dbsource1 -OutFile $dbdestination1
+Invoke-WebRequest $dbsource2 -OutFile $dbdestination2
