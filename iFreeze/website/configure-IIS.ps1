2da0e9675f2666a001ee877f0d7442c501cd5ec9
Chris <christophe.lams@synergics.be>
2018-01-01 15:22:25 +0100

    script update

--- iFreeze/website/configure-IIS.ps1
+++ iFreeze/website/configure-IIS.ps1
@@ -1,4 +1,7 @@
 Add-WindowsFeature Web-Server
+Start-Sleep -s 10
+Remove-Item -Path C:\inetpub\wwwroot\Default.html -Force -
 $dbsource = "https://ifreezwebartifacts.blob.core.windows.net/downloads/default.html"
-$dbdestination = "C:\inetpub\wwwroot\default.html"
+$dbdestination = "C:\inetpub\wwwroot\Default.html"
 Invoke-WebRequest $dbsource -OutFile $dbdestination
+Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
