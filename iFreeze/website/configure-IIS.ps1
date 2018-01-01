2f598229c475c2c71ae21800c999c914b5d2463a
Chris <christophe.lams@synergics.be>
2018-01-01 16:15:55 +0100

    script update

--- iFreeze/website/configure-IIS.ps1
+++ iFreeze/website/configure-IIS.ps1
@@ -4,4 +4,3 @@ Remove-Item -path "C:\inetpub\wwwroot" -Force -Recurse -ErrorAction SilentlyCont
 $dbsource = "https://ifreezwebartifacts.blob.core.windows.net/downloads/default.html"
 $dbdestination = "C:\inetpub\wwwroot\Default.html"
 Invoke-WebRequest $dbsource -OutFile $dbdestination
-Add-Content -Path "C:\inetpub\wwwroot\Default.htm" -Value $($env:computername)
