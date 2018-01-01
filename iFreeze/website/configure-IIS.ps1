08ea072c11e9c218326c85d3caaaf6cf2874fdbc
Chris <christophe.lams@synergics.be>
2018-01-01 18:08:14 +0100

    script updates

--- iFreeze/website/configure-IIS.ps1
+++ iFreeze/website/configure-IIS.ps1
@@ -1,8 +1,1 @@
-Add-WindowsFeature Web-Server
-Remove-Item C:\inetpub\wwwroot\* -Force -Recurse -ErrorAction SilentlyContinue
-$dbsource1 = "https://ifreezwebartifacts.blob.core.windows.net/downloads/default.html"
-$dbsource2 = "https://ifreezwebartifacts.blob.core.windows.net/downloads/ifreezewebsite.gif"
-$dbdestination1 = "C:\inetpub\wwwroot\default.html"
-$dbdestination2 = "C:\inetpub\wwwroot\ifreezewebsite.gif"
-Invoke-WebRequest $dbsource1 -OutFile $dbdestination1
-Invoke-WebRequest $dbsource2 -OutFile $dbdestination2
+Install-WindowsFeature -name Web-Server -IncludeManagementTools
